5bddd797ebdc7c627f72bb571a217cd1ae50a3c3
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-06-01 17:39:45 +0600

    1 file modified

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -14,29 +14,29 @@ export class BookListComponent implements OnInit {
   };
 
   tableData: any[] = [
-    { id: 1, bookName: 'Are You There, Vodka?', price: 1.0079, author: 'A' },
+    { id: 1, bookName: 'Are You There, Vodka?', price: 200, author: 'A' },
     {
       id: 2,
       bookName: 'The Devil Wears Prada',
-      price: 4.0026,
+      price: 100,
       author: 'B'
     },
     {
       id: 3,
       bookName: 'The Curious Incident of the Dog',
-      price: 6.941,
+      price: 400,
       author: 'C'
     },
     {
       id: 4,
       bookName: 'How to Win Friends and Influence',
-      price: 9.0122,
+      price: 300,
       author: 'D'
     },
     {
       id: 5,
       bookName: 'Cloudy with a Chance of Meatballs',
-      price: 10.811,
+      price: 200,
       author: 'E'
     }
   ];
