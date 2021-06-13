b959fa3fec8a7ccc9beec6a53f2248544d0513b4
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-06-13 16:22:46 +0600

    2 files modified

--- src/app/user-list/service/user-list.service.ts
+++ src/app/user-list/service/user-list.service.ts
@@ -6,55 +6,75 @@ import { of } from 'rxjs';
 })
 export class UserListService {
   tableData: any[] = [
-    { id: 1, userName: 'Shakib', userEmail: 'a@gmail.com', age: '30' },
+    {
+      id: 1,
+      userName: 'Shakib',
+      userEmail: 'a@gmail.com',
+      age: '30',
+      isEditable: false
+    },
     {
       id: 2,
       userName: 'Devil',
       userEmail: 'a@gmail.com',
-      age: '25'
+      age: '25',
+      canEdit: false
     },
     {
       id: 3,
       userName: 'Iron-Man',
       userEmail: 'a@gmail.com',
-      age: '20'
+      age: '20',
+      canEdit: false
     },
     {
       id: 4,
       userName: 'Wonder-Woman',
       userEmail: 'a@gmail.com',
-      age: '30'
+      age: '30',
+      canEdit: true
     },
     {
       id: 5,
       userName: 'Super-Man',
       userEmail: 'a@gmail.com',
-      age: '40'
+      age: '40',
+      canEdit: true
     },
-    { id: 6, userName: 'Shakib', userEmail: 'a@gmail.com', age: '30' },
+    {
+      id: 6,
+      userName: 'Shakib',
+      userEmail: 'a@gmail.com',
+      age: '30',
+      canEdit: true
+    },
     {
       id: 7,
       userName: 'Devil',
       userEmail: 'a@gmail.com',
-      age: '25'
+      age: '25',
+      canEdit: true
     },
     {
       id: 8,
       userName: 'Iron-Man',
       userEmail: 'a@gmail.com',
-      age: '40'
+      age: '40',
+      canEdit: true
     },
     {
       id: 9,
       userName: 'Wonder-Woman',
       userEmail: 'a@gmail.com',
-      age: '40'
+      age: '40',
+      canEdit: true
     },
     {
       id: 10,
       userName: 'Super-Man',
       userEmail: 'a@gmail.com',
-      age: '40'
+      age: '40',
+      canEdit: false
     }
   ];
 
